4fc27028431840d77a1eaf565b6fc95fd23e6c16
AFmartinezF <anmartinezf@unal.edu.co>
2024-07-06 13:21:48 -0500

    Add .sh for mpi basic

--- 14-MPI/mpi_basic.cpp
+++ 14-MPI/mpi_basic.cpp
@@ -10,12 +10,15 @@ int main(int argc, char **argv)
 {
   int processId;                 /* rank of process */
   int noProcesses;               /* number of processes */
+  int nameSize;                  /* lenght of name */
+  char computerName[MPI_MAX_PROCESSOR_NAME];
 
   MPI_Init(&argc, &argv);                   /* Mandatory */
   MPI_Comm_size(MPI_COMM_WORLD, &noProcesses);
   MPI_Comm_rank(MPI_COMM_WORLD, &processId);
+  MPI_Get_processor_name(computerName, &nameSize);
 
-  std::fprintf(stdout, "Hello from process %d of %d\n", processId, noProcesses);
+  std::fprintf(stdout, "Hello from process %d of %d, on %s\n", processId, noProcesses, computerName);
 
   MPI_Finalize();                       /* Mandatory */
 
