61cb0cab7987b823996b1eb1c6698306c78e147b
AFmartinezF <anmartinezf@unal.edu.co>
2024-08-23 09:49:11 -0500

    Final version of identity matrix code

--- 14-MPI/identity_matrix.cpp
+++ 14-MPI/identity_matrix.cpp
@@ -27,8 +27,8 @@ std::vector<int> identityRows(int N, int init_row, int final_row){
     //Llenar con unos donde corresponda
     int j = init_row;
     for (int i=0; i<size; ++i){
-	rows[i*N+j] = 1;
-	j++;
+        rows[i*N+j] = 1;
+        j++;
     }
 
     return rows;
@@ -51,30 +51,30 @@ void identityMpi(int N, int pid, int np){
             for (int j = 0; j < N; ++j) {
                 std::cout << identity_local[i * N + j] << " ";
             }
-            std::cout << std::endl;
+            std::cout<<"   Proceso: "<< pid << std::endl;
         }
 
         for(int ipid = 1; ipid < np; ipid++) {
-	    //Recibir los datos de los demas procesos
-	    std::vector<int> recv_rows(Nlocal*N);
-	    double starttime = MPI_Wtime();
+            //Recibir los datos de los demas procesos
+            std::vector<int> recv_rows(Nlocal*N);
+            double starttime = MPI_Wtime();
             MPI_Recv(recv_rows.data(), recv_rows.size(), MPI_INT, ipid, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
             double endtime = MPI_Wtime();
             t = endtime - starttime;
             tam = recv_rows.size() * sizeof(int);  // Tamaño en bytes del mensaje
-            bandwidth += tam /((t/2)*(1024*1024));  // Bandwidth en MB/s
-	    //Imprimir los datos
-	    for (int i = 0; i < Nlocal; ++i) {
+            bandwidth += tam /(t*(1024*1024));  // Bandwidth en MB/s
+            //Imprimir los datos
+            for (int i = 0; i < Nlocal; ++i) {
                 for (int j = 0; j < N; ++j) {
                      std::cout << recv_rows[i * N + j] << " ";
                      }
-                std::cout << std::endl;
+                std::cout<<"   Proceso: "<< ipid << std::endl;
                 }
-	    }
-	//Calculo de ancho de banda promedio
-	bandwidth = bandwidth/np;
-	std::cout<<"El ancho de banda es: "<<bandwidth<<" (MB/s)"<<std::endl;
+            }
+        //Calculo de ancho de banda promedio
+        bandwidth = bandwidth/np;
+        std::cout<<"El ancho de banda es: "<<bandwidth<<" (MB/s)"<<std::endl;
         } else {
        //Enviar el trozo de matriz al pid 0
        MPI_Send(identity_local.data(), identity_local.size(), MPI_INT, 0, tag, MPI_COMM_WORLD);
